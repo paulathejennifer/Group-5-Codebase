55cea20160f96f411ea1c8fba97fcebabe7acf0a
paulathejennifer <paulathejennifer@gmail.com>
2025-06-16 09:12:11 +0300

    Javacsript: Maximum difference in Increaing integers in an array

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ function maximumDifference(arr){
 
 
     let ans = -1;
-    for (let i =0; i < n; i+=)
+    for (let i =0; i < n; i++){
+        for (let j = i + 1; j< n; j++){
+            if (arr[j] > arr[i]){
+                ans = Math.max(ans, arr[j] - arr[i]);
+
+            }
+        }
+    }
+
+    return ans;
 }
+
+let arr = [2,6,1,8,9,35,8,2];
+console.log(maximumDifference(arr));
