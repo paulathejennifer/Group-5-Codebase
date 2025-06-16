26258b81408b90a942c49f94b2644c6c16393206
paulathejennifer <paulathejennifer@gmail.com>
2025-06-16 09:17:49 +0300

    Python: Maximum difference in Increaing integers in an array

---
+++ maxDiff.py
@@ -0,0 +1,15 @@
+def maxDiff(arr){
+#     n = len(arr)
+
+#     ans = -1
+
+#     for i in range(n):
+#         for j in range (i + 1, n):
+#             if arr[j] > arr[i]:
+
+#                 ans = max(ans, arr[j] - arr[i])
+# }
+
+#     return ans
+
+
